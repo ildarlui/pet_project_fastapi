5ad9cd1fa168c6849c73fdfba73fc503afab389e
ildarahtamov <ildar.akhtyamov@sbermarket.ru>
2022-08-03 11:36:30 +0500

    add project

---
+++ db/jobs.py
@@ -0,0 +1,24 @@
+import sqlalchemy
+
+from .base import metadata
+import datetime
+
+jobs = sqlalchemy.Table(
+    "jobs",
+    metadata,
+    sqlalchemy.Column(
+        "id", sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True
+    ),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"), nullable=False),
+    sqlalchemy.Column("title", sqlalchemy.String),
+    sqlalchemy.Column("discription", sqlalchemy.String),
+    sqlalchemy.Column("salary_from", sqlalchemy.Integer),
+    sqlalchemy.Column("salary_to", sqlalchemy.Integer),
+    sqlalchemy.Column("is_company", sqlalchemy.Boolean),
+    sqlalchemy.Column(
+        "created_at", sqlalchemy.DateTime, default=datetime.datetime.utcnow
+    ),
+    sqlalchemy.Column(
+        "updated_at", sqlalchemy.DateTime, default=datetime.datetime.utcnow
+    ),
+)
